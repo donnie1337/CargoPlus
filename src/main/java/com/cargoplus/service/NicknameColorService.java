c47cdc2d35f3a03be820bc1cc9de315f0487fd9d
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-20 08:39:43 -0300

    fix(nametag): reforca opacidade total das cores

--- src/main/java/com/cargoplus/service/NicknameColorService.java
+++ src/main/java/com/cargoplus/service/NicknameColorService.java
@@ -144,6 +144,7 @@ public final class NicknameColorService {
             display.setDefaultBackground(false);
             display.setBackgroundColor(org.bukkit.Color.fromARGB(0, 0, 0, 0));
             display.setShadowed(false);
+            display.setTextOpacity((byte) 100);
             display.setBrightness(new Display.Brightness(15, 15));
             display.setSeeThrough(true);
             display.setGravity(false);
