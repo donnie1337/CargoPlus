19e77eb1b5ffc5249247ca323e6ab6db01a5e24e
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-20 09:49:43 -0300

    feat(nametag): reduz opacidade ao agachar

--- src/main/java/com/cargoplus/service/NicknameColorService.java
+++ src/main/java/com/cargoplus/service/NicknameColorService.java
@@ -145,7 +145,7 @@ public final class NicknameColorService {
             display.setDefaultBackground(false);
             display.setBackgroundColor(org.bukkit.Color.fromARGB(0, 0, 0, 0));
             display.setShadowed(true);
-            display.setTextOpacity((byte) 100);
+            display.setTextOpacity((byte) (player.isSneaking() ? 40 : 100));
             display.setBrightness(new Display.Brightness(15, 15));
             display.setSeeThrough(true);
             display.setGravity(false);
@@ -172,7 +172,7 @@ public final class NicknameColorService {
         display.setBrightness(new Display.Brightness(15, 15));
         display.setSeeThrough(true);
         display.setText(renderedName);
-        display.setTextOpacity((byte) 100);
+        display.setTextOpacity((byte) (player.isSneaking() ? 40 : 100));
         refreshNametagVisibility(player, display);
         scheduleNametagVisibilityRefresh(player, display);
     }
