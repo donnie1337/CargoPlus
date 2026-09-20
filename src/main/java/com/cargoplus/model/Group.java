384cb1e93b4406756f8fff5ff79d2a0b3cfdae93
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-20 07:53:03 -0300

    feat(groups): adiciona configuracao de glow por cargo

--- src/main/java/com/cargoplus/model/Group.java
+++ src/main/java/com/cargoplus/model/Group.java
@@ -2,7 +2,7 @@ package com.cargoplus.model;
 
 import java.util.List;
 
-public record Group(String name, String displayName, String prefix, String nameColor, List<String> permissions, List<String> parents) {
+public record Group(String name, String displayName, boolean glow, String prefix, String nameColor, List<String> permissions, List<String> parents) {
     public Group {
         name = name == null ? "" : name;
         displayName = displayName == null ? name : displayName;
